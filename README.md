5444e0e60d101a13c06f147a45007132e9a8e847
Miguel-Oliveiraa <miguellgomes64@gmail.com>
2025-03-13 00:41:18 -0300

    feat: up & running

---
+++ README.md
@@ -0,0 +1,32 @@
+# Chatbot-hacker-cidadao
+
+## Pré-requisitos
+
+1. Certifique-se de ter o Node.js e o npm instalados em sua máquina.
+2. Certifique-se de ter o Python instalado em sua máquina.
+
+## Passos para Rodar a Aplicação
+
+1. Clone o repositório `x` e rode a aplicação em Python:
+
+```bash
+git clone https://github.com/EllianRodrigues/HACKER-CIDADAO-12.0.git
+cd HACKER-CIDADAO-12.0.git
+python source.py
+```
+
+2. No diretório do projeto atual, instale as dependências do Node.js:
+
+```bash
+npm install
+```
+
+3. Inicie a aplicação:
+
+```bash
+npm start
+```
+
+4. Leia o qrcode do terminal no seu whatsapp.
+
+Pronto! A aplicação estará rodando e pronta para uso.
--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import { Client } from "whatsapp-web.js";
 import qrcode from "qrcode-terminal";
 import OpenAI from "openai";
 import "dotenv/config"; // This will automatically load variables from .env
+import axios from "axios";
+
+const parentsData = await axios.get("http://127.0.0.1:5000/dados");
+const data = JSON.parse(JSON.stringify(parentsData.data));
 
 const chatgptClient = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
@@ -11,6 +15,64 @@ const client = new Client();
 
 client.on("ready", () => {
   console.log("Client is ready!");
+  data.forEach((element) => {
+    client.sendMessage(
+      `${element.telefone}@c.us`,
+      `Oi, tudo bem, ${element.Nome}? 😊
+
+Sou o chatbot da Prefeitura e estou aqui para te ajudar! Vi que ${
+        element.Filhos[0]
+      } está na fase de entrar na ${element.escolas_proximas[0][
+        `Tipo`
+      ].toLowerCase()}, um momento super importante no desenvolvimento dele(a). 💙 Para facilitar essa escolha, selecionei uma lista das ${element.escolas_proximas[0][
+        `Tipo`
+      ].toLowerCase()} mais próximas de você, organizadas pelas avaliações institucionais.
+
+📍 Aqui estão as três melhores opções perto de você:
+
+1️⃣ ${element.escolas_proximas[0][`Tipo`]} ${
+        element.escolas_proximas[0][`Nome da Escola`]
+      }
+      ⭐ SAEB: ${
+        element.escolas_proximas[0][`Nota SAEB - 2023 Media`] == undefined
+          ? "Não disponível"
+          : element.escolas_proximas[0][`Nota SAEB - 2023 Media`]
+      } | 🚍 Transporte gratuito: ${
+        element.escolas_proximas[0][`Transporte`] ===
+        "nao ha dados de transporte publico para essa escola"
+          ? "Não"
+          : "Sim"
+      }
+2️⃣ ${element.escolas_proximas[1][`Tipo`]} ${
+        element.escolas_proximas[1][`Nome da Escola`]
+      }
+      ⭐ SAEB: ${
+        element.escolas_proximas[1][`Nota SAEB - 2023 Media`] == undefined
+          ? "Não disponível"
+          : element.escolas_proximas[1][`Nota SAEB - 2023 Media`]
+      } | 🚍 Transporte gratuito: ${
+        element.escolas_proximas[1][`Transporte`] ===
+        "nao ha dados de transporte publico para essa escola"
+          ? "Não"
+          : "Sim"
+      }
+3️⃣ ${element.escolas_proximas[2][`Tipo`]} ${
+        element.escolas_proximas[2][`Nome da Escola`]
+      }
+      ⭐ SAEB: ${
+        element.escolas_proximas[2][`Nota SAEB - 2023 Media`] == undefined
+          ? "Não disponível"
+          : element.escolas_proximas[2][`Nota SAEB - 2023 Media`]
+      } | 🚍 Transporte gratuito: ${
+        element.escolas_proximas[2][`Transporte`] ===
+        "nao ha dados de transporte publico para essa escola"
+          ? "Não"
+          : "Sim"
+      }
+
+Se quiser mais detalhes sobre alguma delas, é só me chamar! Estou aqui para te ajudar nessa decisão. 😉`
+    );
+  });
 });
 
 client.on("qr", (qr) => {
@@ -18,26 +80,37 @@ client.on("qr", (qr) => {
 });
 
 client.on("message_create", async (message) => {
-  if (message.body) {
-    // const message = "Qual o maior predio do mundo?";
-    const completion = await chatgptClient.chat.completions.create({
-      model: "gpt-4o-mini",
-      messages: [
-        {
-          role: "user",
-          content: `_id,rpa,tipo_cod,tipo,cod_escola,escola,inep,rua,numero,cod_bairro,bairro,metragem,qtd_alunos,qtd_turmas,qtd_professores,escola_climatizada,data_visita,quadra_coberta,quadra_descoberta,biblioteca,sala_recurso,gestor,longitude,latitude
-          1,1,1,ESCOLA MUNICIPAL,90,ALMIRANTE SOARES DUTRA,26120658,CAMUTANGA,180,51,CABANGA,"764,3",429,18,13,SIM,2018-02-27T00:00:00,NÃO,NÃO,SIM,SIM,VERONICA MARIA LIMA,-34.89535,-8.07895
-          2,1,1,ESCOLA MUNICIPAL,289,CIDADAO HERBERT DE SOUZA,26121972,ARNOBIO MARQUES,310,108,SANTO AMARO,206,163,12,11,NÂO,,NÃO,NÃO,NÃO,SIM,ANA CLAUDIA DO NASCIMENTO SILVA,-34.88707,-8.04576
-          3,1,1,ESCOLA MUNICIPAL,256,DO COQUE,26122898,MIRANDOPOLIS,35,43,ILHA JOANA BEZERRA,"840,5",622,26,18,NÂO,,NÃO,NÃO,SIM,SIM,GRIJALBA MARIA PESSOA,-34.90022,-8.07145      
-          ${message.body}`,
-        },
-      ],
-    });
-
-    const response = completion.choices[0].message.content;
+  if (message.body.startsWith("!")) {
+    const telefone = message.from.split("@")[0];
+    // Example usage
+    const dataset = data.find((parent) => parent.telefone === telefone);
+    // const response = completion.choices[0].message.content;
+    const response = await answerQuestion(message.body.split("!")[1], dataset);
     // send back "pong" to the chat the message was sent in
     client.sendMessage(message.from, response);
   }
 });
+
+async function answerQuestion(question, dataset) {
+  // const context = await createContext(question, dataset, maxLen);
+  const context = JSON.stringify(dataset);
+
+  try {
+    const response = await chatgptClient.chat.completions.create({
+      model: "gpt-4o-mini",
+      messages: [
+        {
+          role: "system",
+          content: `Responda as perguntas do usuario (chame-o pelo nome, que esta no campo "Nome"), sobre escolas e creches com base no contexto abaixo, caso ele pergunte como matricular o filho em uma escola ou creche responda com "📌 Como fazer a matrícula em uma escola ou creche municipal do Recife? \n\n Para matricular seu filho, acesse o portal Matrícula Online Recife através do site \n\n 👉 https://matriculaonline.recife.pe.gov.br \n\n Lá, você pode verificar a disponibilidade de vagas e obter informações atualizadas sobre todo o processo. ✅📚", e se a pergunta não puder ser respondida diga "Desculpe, eu ainda não sei responder isso. Teria outra coisa que eu posso ajudar? 🙂"\n\nContexto: ${context}\n\n---\n\nPergunta: ${question}\nResposta:`,
+        },
+      ],
+    });
+
+    return response.choices[0].message.content;
+  } catch (error) {
+    console.error(error);
+    return "";
+  }
+}
 
 client.initialize();
